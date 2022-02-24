1f24b7c899082309e4e6be94cd506b4c3bc37a81
Gianinaaa1 <99227174+Gianinaaa1@users.noreply.github.com>
2022-02-24 22:09:48 +0700

    Add files via upload

---
+++ Bubblesort_gia.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void bubbleSort(int arr[], int n){
+  int i, j, tmp;
+  for(i = 0; i < n; i++){
+    for(j=0; j < n-i-1; j ++){
+      if(arr[j] > arr[j+1]){
+        tmp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = tmp;
+      }
+    }
+  }
+}
+int main() {
+  int array[100], n, i, j;
+  printf("Masukkan jumlah data: ");
+  scanf("%d", &n);
+  printf("\nMasukkan nilai: \n");
+  for(i = 0; i < n; i++){
+    scanf("%d", &array[i]);
+  }
+  bubbleSort(array, n);
+  printf("\nHasil pengurutan metode Ascending sebagai berikut:\n");
+  for(i = 0; i < n; i++){
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
